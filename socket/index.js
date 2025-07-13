db4adef5c340179663d4d77e1d0c1f32ba2e6e02
Nishant Rathore <rnishant609@gmail.com>
2025-07-13 12:20:07 +0530

    d

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,7 @@ import { Server } from 'socket.io';
 
 const io = new Server(9000, {
     cors: {
-        origin: 'https://watsapp-clone-client.vercel.app',
+        origin: 'http://localhost:8000',
     },
 })
 
